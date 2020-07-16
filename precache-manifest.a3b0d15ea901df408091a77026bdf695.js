d64d9bd1498ed1fa02d4595e28d83b038b02fc9a
4teenth <aio.ao91@gmail.com>
2020-07-17 01:24:17 +0300

    Updates

---
+++ precache-manifest.a3b0d15ea901df408091a77026bdf695.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2392468b93326f3c0671fd3aa1cb696",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "03ee53d83ea90dcf49d5",
+    "url": "/goit-react-hw-01-components/static/css/2.972ec59c.chunk.css"
+  },
+  {
+    "revision": "3131dec9c75942b6a477",
+    "url": "/goit-react-hw-01-components/static/css/main.37a9a78f.chunk.css"
+  },
+  {
+    "revision": "03ee53d83ea90dcf49d5",
+    "url": "/goit-react-hw-01-components/static/js/2.4d523248.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.4d523248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3131dec9c75942b6a477",
+    "url": "/goit-react-hw-01-components/static/js/main.695cbec4.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  },
+  {
+    "revision": "37143a5695c648b9f663dc9a0b26d1d9",
+    "url": "/goit-react-hw-01-components/static/media/userlogo.37143a56.jpg"
+  }
+]);
